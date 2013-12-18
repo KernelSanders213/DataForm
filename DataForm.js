6b14473b429faa47498340b8fbb9297f3e42b0af
KernelSanders213 <jpking5191@gmail.com>
2013-12-18 13:52:38 -0500

    Update DataForm.js
    It is now completely dynamic. This introduces the new namespace perForm. Also finish encapsulating running a function before and after a submit.
    New attributes and renamed:
    *type - this is the type of the data attribute form, "AjaxUpdate" or "Form"
    *bFunction - functions that run before a submit
    *aFunction - functions that run after a submit
    *location - location to post the data
    *form - the Form's name
    *target - the target to update

--- DataForm.js
+++ DataForm.js
@@ -1,5 +1,10 @@
-$('[data-submitOnChange]').change(function () {
-        if ($(this).attr('data-runFunction')) window[$(this).attr('data-runFunction')]();
-        if ($(this))
-        $("#validationStatus").load("@UrlPathManager.UrlPath" + "Home/ValidationStatusPartial", $("#"+$(this).attr('data-formName')).serializeArray());
+    $('[data-perForm-type]').change(function () {
+        if ($(this).attr('data-perForm-bFunction')) window[$(this).attr('data-perForm-bFunction')]();
+        if ($(this).attr('[data-perForm-type]') == "AjaxUpdate"){
+            $("#"+$(this).attr('data_perForm_target')).load($(this).attr('data_perForm_location'), $("#" + $(this).attr('data-perForm-form')).serializeArray());
+        }
+        if ($(this).attr('[data-perForm-type]') == "Form") {
+            $("#" + $(this).attr('data-perForm-form')).submit();
+        }
+        if ($(this).attr('data-perForm-aFunction')) window[$(this).attr('data-perForm-aFunction')]();
     });
