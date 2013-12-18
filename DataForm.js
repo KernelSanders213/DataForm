30533a12510e8603af579451aa99d917b256f216
KernelSanders213 <jpking5191@gmail.com>
2013-12-18 16:43:02 -0500

    Update DataForm.js
    Fix jQuery selectors for attributes

--- DataForm.js
+++ DataForm.js
@@ -1,9 +1,9 @@
-    $('[data-perForm-type]').change(function () {
+$('[data-perForm-type]').change(function () {
         if ($(this).attr('data-perForm-bFunction')) window[$(this).attr('data-perForm-bFunction')]();
-        if ($(this).attr('[data-perForm-type]') == "AjaxUpdate"){
-            $("#"+$(this).attr('data_perForm_target')).load($(this).attr('data_perForm_location'), $("#" + $(this).attr('data-perForm-form')).serializeArray());
+        if ($(this).attr('data-perForm-type') == "AjaxUpdate") {
+            $("#" + $(this).attr('data-perForm-target')).load($(this).attr('data-perForm-location'), $("#" + $(this).attr('data-perForm-form')).serializeArray());
         }
-        if ($(this).attr('[data-perForm-type]') == "Form") {
+        if ($(this).attr('data-perForm-type') == "Form") {
             $("#" + $(this).attr('data-perForm-form')).submit();
         }
         if ($(this).attr('data-perForm-aFunction')) window[$(this).attr('data-perForm-aFunction')]();
