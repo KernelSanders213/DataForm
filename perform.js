e97dbc6b52f5877bba8dac951fe9509f1d204db2
SnareChops <snare_chops@yahoo.com>
2014-01-13 02:08:26 -0500

    Removing now unneeded code

--- perform.js
+++ perform.js
@@ -180,66 +180,6 @@ $(function () {
             });
 
         },
-        /*runBeforeFns: function(item, q){
-            var continueSubmit = true;
-            if (item.befores !== undefined) {
-                var bFunctions = item.befores[q].split(perform.split.secondary);
-                var bParams = item.beforeparams[q].split(perform.split.secondary);
-                for(var i in bFunctions){
-                    var fn = bFunctions[i];
-                    var returnVal;
-                    if (fn !== ''){
-                        var params = bParams[i].split(perform.split.tertiary);
-                        if (params === undefined || params == '') returnVal = window[fn]();
-                        else returnVal = window[fn].apply(this, params);
-                    }
-                    if(returnVal === false) continueSubmit = false;
-                }
-            }
-            return continueSubmit;
-        },*/
-        /*runAfterFns: function(item, q){
-            if (item.afters !== undefined) {
-                var aFunctions = item.afters[q].split(perform.split.secondary);
-                var aParams = item.afterparams[q].split(perform.split.secondary);
-                for(var i in aFunctions){
-                    var fn = aFunctions[i];
-                    if (fn !== ''){
-                        var params = aParams[i].split(perform.split.tertiary);
-                        if (params === undefined || params == '') window[fn]();
-                        else window[fn].apply(this, params);
-                    }
-                }
-            }
-        },*/
-        /*runSuccessFns: function(item, q){
-            if (item.successfns !== undefined) {
-                var sFunctions = item.successfns[q].split(perform.split.secondary);
-                var sParams = item.successfnparams[q].split(perform.split.secondary);
-                for(var i in sFunctions){
-                    var fn = sFunctions[i];
-                    if (fn !== ''){
-                        var params = sParams[i].split(perform.split.tertiary);
-                        if (params === undefined || params == '') window[fn]();
-                        else window[fn].apply(this, params);
-                    }
-                }
-            }
-        },*/
-        /*runErrorFns: function(item, q){
-            if (item.errorfns !== undefined) {
-                var eFunctions = item.errorfns[q].split(perform.split.secondary);
-                var eParams = item.errorfnparams[q].split(perform.split.secondary);
-                for(var i in eFunctions){
-                    var fn = eFunctions[i];
-                    if (fn !== ''){
-                        var params = eParams[i].split(perform.split.tertiary);
-                        if (params === undefined || params == '') window[fn]();
-                        else window[fn].apply(this, params);
-                    }
-                }
-            }
-        },*/
         reset: function () {
             perform.collection = [];
         },
@@ -272,27 +212,6 @@ $(function () {
             if (perform.verbose && (!attr || attr === undefined)) return;
             return attr ? attr.split(perform.split) : [];
         },
-        /*getBefores: function (submit, event) {
-            return submit.attr('data-perform-bfns') ? submit.attr('data-perform-bfns').split(perform.split.main) : undefined;
-        },*/
-        /*getAfters: function (submit, event) {
-            return submit.attr('data-perform-afns') ? submit.attr('data-perform-afns').split(perform.split.main) : undefined;
-        },*/
-        /*getAfterParams: function (submit, event) {
-            return submit.attr('data-perform-aparams') ? submit.attr('data-perform-aparams').split(perform.split.main) : undefined;
-        },*/
-        /*getSuccessFns: function (submit) {
-            return submit.attr('data-perform-sfns') ? submit.attr('data-perform-sfns').split(perform.split.main) : undefined;
-        },*/
-        /*getSuccessFnParams: function (submit) {
-            return submit.attr('data-perform-sparams') ? submit.attr('data-perform-sparams').split(perform.split.main) : undefined;
-        },*/
-        /*getErrorFns: function (submit) {
-            return submit.attr('data-perform-efns') ? submit.attr('data-perform-efns').split(perform.split.main) : undefined;
-        },*/
-        /*getErrorFnParams: function (submit) {
-            return submit.attr('data-perform-eparams') ? submit.attr('data-perform-eparams').split(perform.split.main) : undefined;
-        },*/
         errorCheck: function(model) {
             var maxCount = 0;
             //Get the max amount of an array
@@ -301,15 +220,7 @@ $(function () {
             model.actions.length >= maxCount ? maxCount = model.actions.length: false;
             model.methods.length >= maxCount ? maxCount = model.methods.length: false;
             model.targets.length >= maxCount ? maxCount = model.targets.length: false;
-            /*if(model.befores !== undefined) model.befores.length >= maxCount ? maxCount = model.befores.length: false;
-            if(model.beforeparams !== undefined) model.beforeparams.length >= maxCount ? maxCount = model.beforeparams.length: false;
-            if(model.afters !== undefined) model.afters.length >= maxCount ? maxCount = model.afters.length: false;
-            if(model.afterparams !== undefined) model.afterparams.length >= maxCount ? maxCount = model.afterparams.length: false;
-            if(model.successfns !== undefined) model.successfns.length >= maxCount ? maxCount = model.successfns.length: false;
-            if(model.successfnparams !== undefined) model.successfnparams.length >= maxCount ? maxCount = model.successfnparams.length: false;
-            if(model.errorfns !== undefined) model.errorfns.length >= maxCount ? maxCount = model.errorfns.length: false;
-            if(model.errorfnparams !== undefined) model.errorfnparams.length >= maxCount ? maxCount = model.errorfnparams.length: false;
-*/            //Log the errors of the improper index count
+            //Log the errors of the improper index count
             if (model.formids.length < maxCount) console.error("Invalid data-perform-forms index count: " + model.formids.length + " of total: " + maxCount
             + ". Please  make sure it is split properly with the character '" + perform.split + "'.");
             if (model.events.length < maxCount) console.error("Invalid data-perform-events index count: " + model.events.length + " of total: " + maxCount
@@ -320,22 +231,6 @@ $(function () {
             + ". Please  make sure it is split properly with the character '" + perform.split + "'.");
             if (model.targets.length < maxCount) console.error("Invalid data-perform-targets index count: " + model.targets.length + " of total: " + maxCount
             + ". Please  make sure it is split properly with the character '" + perform.split + "'.");
-            /*if (model.befores !== undefined && model.befores.length < maxCount) console.error("Invalid data-perform-bfns index count: " + model.befores.length + " of total: " + maxCount
-            + ". Please  make sure it is split properly with the character '" + perform.splitchar + "'.");
-            if (model.beforeparams !== undefined && model.beforeparams.length < maxCount) console.error("Invalid data-perform-bparams index count: " + model.beforeparams.length + " of total: " + maxCount
-            + ". Please  make sure it is split properly with the character ';'.");
-            if (model.afters !== undefined && model.afters.length < maxCount) console.error("Invalid data-perform-afns index count: " + model.afters.length + " of total: " + maxCount
-            + ". Please  make sure it is split properly with the character '" + perform.splitchar + "'.");
-            if (model.afterparams !== undefined && model.afterparams.length < maxCount) console.error("Invalid data-perform-aparams index count: " + model.afterparams.length + " of total: " + maxCount
-            + ". Please  make sure it is split properly with the character ';'.");
-            if (model.successfns !== undefined && model.successfns.length < maxCount) console.error("Invalid data-perform-sfns index count: " + model.successfns.length + " of total: " + maxCount
-            + ". Please  make sure it is split properly with the character '" + perform.splitchar + "'.");
-            if (model.successfnparams !== undefined && model.successfnparams.length < maxCount) console.error("Invalid data-perform-sparams index count: " + model.successfnparams.length + " of total: " + maxCount
-            + ". Please  make sure it is split properly with the character ';'.");
-            if (model.errorfns !== undefined && model.errorfns.length < maxCount) console.error("Invalid data-perform-efns index count: " + model.errorfns.length + " of total: " + maxCount
-            + ". Please  make sure it is split properly with the character '" + perform.splitchar + "'.");
-            if (model.errorfnparams !== undefined && model.errorfnparams.length < maxCount) console.error("Invalid data-perform-eparams index count: " + model.errorfnparams.length + " of total: " + maxCount
-            + ". Please  make sure it is split properly with the character ';'.");*/
         }
     };
     perform.parse();
